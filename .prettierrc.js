90337c629a0bca46e5e9b4bff977a77510372662
vvvliqian917 <vvvliqian@163.com>
2022-04-26 21:18:51 +0800

    feat: npmignore

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Comment item is the default configuration
+module.exports = {
+  printWidth: 100,
+  // tabWidth: 2,
+  // useTabs: false,
+  semi: false,
+  singleQuote: true,
+  // jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  // "babel" (via @babel/parser) Named "babylon" until v1.16.0
+  // parser: 'babylon',
+};
